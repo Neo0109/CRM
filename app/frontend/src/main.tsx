78ff1a59ccae984ca526d540381ca4015b3cab41
Neo0109 <128599394+Neo0109@users.noreply.github.com>
2026-05-25 12:19:18 +0800

    Mount review queue visibility behavior

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ManualLeadLauncher } from "./ManualLeadLauncher";
+import { ReviewQueueBehavior } from "./ReviewQueueBehavior";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <ManualLeadLauncher />
+    <ReviewQueueBehavior />
   </React.StrictMode>
 );
