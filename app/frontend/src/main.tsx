f4c4f3af3c1bab6aa4ee9a2e59864b124056e81e
Neo0109 <128599394+Neo0109@users.noreply.github.com>
2026-05-25 14:41:34 +0800

    Mount v1.3.2 detail UX refinements

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { CalendarLauncher } from "./CalendarLauncher";
+import { DetailUxRefinement } from "./DetailUxRefinement";
 import { ManualLeadLauncher } from "./ManualLeadLauncher";
 import { ReviewQueueBehavior } from "./ReviewQueueBehavior";
 import { SteamStoreLinkBehavior } from "./SteamStoreLinkBehavior";
 import "./styles.css";
 import "./calendar.css";
 import "./calendar-refinement.css";
+import "./detail-ux-refinement.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -16,5 +18,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <ManualLeadLauncher />
     <ReviewQueueBehavior />
     <SteamStoreLinkBehavior />
+    <DetailUxRefinement />
   </React.StrictMode>
 );
