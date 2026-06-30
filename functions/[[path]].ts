b3916469d82a5c47fc2f45c6d9896b563d718c32
Neo0109 <128599394+Neo0109@users.noreply.github.com>
2026-06-30 18:10:08 +0200

    Document product version governance catch-up

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -2,7 +2,7 @@ import type { PagesContext } from "./_lib/crm";
 
 const stylesheetFile = "index.css";
 const scriptFile = "index.js";
-const assetVersion = "20260604-automation-diagnostics-center-v24";
+const assetVersion = "20260630-version-governance-catchup-v25";
 
 function versionedAsset(fileName: string) {
   return `/assets/${fileName}?v=${assetVersion}`;
